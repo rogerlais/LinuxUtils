b1a21cba578ab7f51f2fb1b135ecfeb9694d1a45
Roger <rogerlais@gmail.com>
2020-10-17 17:36:59 -0300

    first functional version FixVBoxSharedFolder.sh

--- FixVBoxSharedFolder.sh
+++ FixVBoxSharedFolder.sh
@@ -3,9 +3,15 @@
 #Corrige necessiade de autenticação para acesso ao shared folder do virtualbox
 
 
-if[ -not $1 ]; then
-	echo "Informe nome de usuário para realizar mapeamento direto de recurso do hospedeiro"
-	exit
+if [ -not $1 ]
+then
+	read -p "Informe nome de usuário para realizar mapeamento direto de recurso do hospedeiro: " grantUser
+	if [ ${grantUser} == "" ]; then
+		exit
+	fi
+else
+	grantUser=${1}
 fi
 
-sudo adduser $1 vboxsf
+echo "Inserido ${grantUser} ao grupo de permissões para pastas compartilhados do VirtualBox"
+sudo adduser ${grantUser} vboxsf
