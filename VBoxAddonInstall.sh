75f39d9a6b53c51ad88197fa498d2c8bcb7ffba7
Roger <rogerlais@gmail.com>
2023-09-01 16:57:40 -0300

    VBoxAddonInstall.sh update

--- VBoxAddonInstall.sh
+++ VBoxAddonInstall.sh
@@ -12,7 +12,7 @@ sudo apt upgrade
 sudo apt install build-essential dkms linux-headers-$(uname -r)
 
 #caso haja problemas com o pacote linux-headers-$(uname -r)
-#Tente executar a linha abaixo
+#Tente executar a linha abaixo. Dependendo da distro, pode ocorrer erro
 sudo apt-get install linux-image-amd64 linux-headers-amd64
 
 #Modo alternativo
@@ -30,7 +30,7 @@ lsmod | grep vboxguest
 sudo /usr/sbin/VBoxService --version
 
 #!Insere a conta atual no grupo de montadores/RW 
-sudo usermod -aG vboxusers $USER
+sudo usermod -aG vboxusers $USER   #Sometimes replies a inexistent group
 sudo usermod -aG vboxsf $USER
 
 #todo: o link abaixo versa sobre o caso do kali-linux possuir diferencas
