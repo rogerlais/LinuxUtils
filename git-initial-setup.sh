677c9f98d6ecb72222f4bffa01f8f0ff4c7d76b1
rogerlais <rogerlais@gmail.com>
2024-04-14 06:51:20 -0300

    First commit fom mint-2023-1

--- git-initial-setup.sh
+++ git-initial-setup.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 #Inicia dados para conta de acesso ao git local/remoto
 
-read -p "Informe nome do usuário:" gituser
-read -p "Informe email de autenticação:" gitemail
+read -rp "Informe nome do usuário:" gituser
+read -rp "Informe email de autenticação:" gitemail
 echo "Iniciando configuração para ${gituser}..."
-echo "Usando conta \""${gitemail}\"""
+echo "Usando conta \"${gitemail}\""
 
-read -p "Pressione enter para continuar..." dummy
+# shellcheck disable=SC2034
+read -rp "Pressione enter para continuar..." dummy
 
 git config --global user.name \""${gituser}\""
 
